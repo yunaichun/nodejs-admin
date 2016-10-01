780d37773adf900f8132c8c9bc7887c9d2a5d5e2
naichunyu <naichuyu@gmail.com>
2016-10-01 17:17:20 +0100

    增加搜索功能，与电影分类公用查询分页模板

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -74,42 +74,71 @@ exports.search=function(req, res) {
     //前台传递的电影分类ID
     var catId=req.query.cat;
     //前台传递的分页页码
-    var page=parseInt(req.query.p,10);
+    var page=parseInt(req.query.p,10)||0;//搜索框查询的时候没有传q默认为0
+
+    //通过搜索框传递的值
+    var q=req.query.q;
     console.log("前台传递分类ID"+catId);
     console.log("前台传递当前页"+page);
     //每页显示的条数
     var count=2;
     //默认显示第一页（从0标记开始）
     var index=page*count;//其中*2是和limit是一样的 
-    
-    Catetory
-        .find({_id:catId})//根据分类id查询
-        //populate第一个参数指的是查询Movies里面的ID，第二个参数是限制为5条
-        .populate({
-            path:'movies',//就只查关联表里面对应的movies
-            select:'title poster'//就只查关联表movies里面的title和poster字段
-            //options:{limit:2,skip:index}//跳过前几条
-        })
-        .exec(function(err,catetories){
-            console.log('根据分类ID查询到的所有电影分类'+catetories);
+
+//查询电影分类
+    if(catId){
+        Catetory
+            .find({_id:catId})//根据分类id查询
+            //populate第一个参数指的是查询Movies里面的ID，第二个参数是限制为5条
+            .populate({
+                path:'movies',//就只查关联表里面对应的movies
+                select:'title poster'//就只查关联表movies里面的title和poster字段
+                //options:{limit:2,skip:index}//跳过前几条
+            })
+            .exec(function(err,catetories){
+                console.log('根据分类ID查询到的所有电影分类'+catetories);
+                if (err) {
+                    console.log(err);
+                }
+                var catetory=catetories[0]||[];
+                //实现分页逻辑
+                var movies=catetory.movies||[];
+                console.log('此电影分类下的所有电影'+movies);
+                //这才是分页的逻辑（前台传递的第几页index+后台规定的每页多少条）
+                var results=movies.slice(index,index+count);
+
+                //只差徐一条数据
+                res.render('results', {
+                    title: '结果列表页面',
+                    keyword:catetory.name,
+                    currentPage:page+1,//当前页
+                    query:'cat='+catId,
+                    totalPage:Math.ceil(movies.length/2),//总页数
+                    movies: results
+                })
+            })
+     }
+//通过搜索框执行的查找
+     else{
+        Movie
+        .find({title:new RegExp(q+'.*','i')})//通过前台输入的q   查询标题为q的电影
+        .exec(function(err,movies){
             if (err) {
-                console.log(err);
+                    console.log(err);
             }
-            var catetory=catetories[0]||[];
-            //实现分页逻辑
-            var movies=catetory.movies||[];
-            console.log('此电影分类下的所有电影'+movies);
+
             //这才是分页的逻辑（前台传递的第几页index+后台规定的每页多少条）
-            var results=movies.slice(index,index+count);
+            var results=movies.slice(index,index+count);//此情况默认index为0
 
             //只差徐一条数据
             res.render('results', {
                 title: '结果列表页面',
-                keyword:catetory.name,
+                keyword:q,//关键词就是前台输入的查询条件
                 currentPage:page+1,//当前页
-                query:'cat='+catId,
+                query:'q='+q,
                 totalPage:Math.ceil(movies.length/2),//总页数
                 movies: results
             })
         })
+     }
 }
