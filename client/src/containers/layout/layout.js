2c2746dbb682121231b09bc504fc4feb23de4d8d
wufang <naichunyu@aliyun.com>
2017-12-29 14:56:09 +0800

    添加首页导航栏

---
+++ client/src/containers/layout/layout.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import '../../style/layout/layout.less';
+
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class LayOut extends React.Component {
+	constructor(props) {
+		super(props);
+		// 作用是访问到this.props和this.state
+		this.onCollapse = this.onCollapse.bind(this);
+		this.logout = this.logout.bind(this);
+		this.state = {
+			collapsed: false,
+		};
+	}
+	onCollapse(collapsed) {
+		console.log(collapsed);
+		this.setState({
+			collapsed: !this.state.collapsed,
+		});
+	}
+	logout() {
+		alert(1);
+		this.props.history.push('/');
+	}
+	render() {
+		return (
+			<Layout style={{ minHeight: '100vh' }}>
+				<Sider
+					collapsible
+					collapsed={this.state.collapsed}
+					onCollapse={this.onCollapse}
+				>
+					<div className="logo" />
+					<Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+					<Menu.Item key="1">
+						<Link to="/">
+							<Icon type="pie-chart" />
+							<span>Option 1</span>
+						</Link>
+					</Menu.Item>
+					<Menu.Item key="2">
+						<Icon type="desktop" />
+						<span>Option 2</span>
+					</Menu.Item>
+					<SubMenu
+						key="sub1"
+						title={<span><Icon type="user" /><span>User</span></span>}
+					>
+						<Menu.Item key="3">Tom</Menu.Item>
+						<Menu.Item key="4">Bill</Menu.Item>
+						<Menu.Item key="5">Alex</Menu.Item>
+					</SubMenu>
+					<SubMenu
+						key="sub2"
+						title={<span><Icon type="team" /><span>Team</span></span>}
+					>
+						<Menu.Item key="6">Team 1</Menu.Item>
+						<Menu.Item key="8">Team 2</Menu.Item>
+					</SubMenu>
+					<Menu.Item key="9">
+						<Icon type="file" />
+						<span>File</span>
+					</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header style={{ background: '#fff', padding: 0 }}>
+						<Icon
+							className="trigger"
+							type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+							onClick={this.onCollapse}
+						/>
+						<Menu mode="horizontal" onClick={this.logout} className="layout-header-menu">
+							<SubMenu title={<span><Icon type="user" />sosout</span>}>
+								<Menu.Item key="logout">注销</Menu.Item>
+							</SubMenu>
+						</Menu>
+					</Header>
+					<Content style={{ margin: '0 16px' }}>
+					<Breadcrumb style={{ margin: '16px 0' }}>
+						<Breadcrumb.Item>User</Breadcrumb.Item>
+						<Breadcrumb.Item>Bill</Breadcrumb.Item>
+					</Breadcrumb>
+					<div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+						Bill is a cat.
+					</div>
+					</Content>
+					<Footer style={{ textAlign: 'center' }}>
+						Ant Design ©2016 Created by Ant UED
+					</Footer>
+				</Layout>
+			</Layout>
+		);
+	}
+}
+
+export default LayOut;
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, HashRouter, Route } from 'react-router-dom';
-
+/*Redux异步Action测试*/
 import reduxThunkAction from '../containers/examples/reduxThunkAction';
 import reduxPromiseAction1 from '../containers/examples/reduxPromiseAction1';
 import reduxPromiseAction2 from '../containers/examples/reduxPromiseAction2';
-
+/*前端页面组件*/
 import Login from '../containers/login/login';
+import LayOut from '../containers/layout/layout';
 
 const RouterConfig = () => (
   <HashRouter>
     <div>
 		<Route exact path="/" component={Login} />
+		<Route path="/home" component={LayOut} />
 		<Route path="/reduxThunkAction" component={reduxThunkAction} />
 		<Route path="/reduxPromiseAction1" component={reduxPromiseAction1} />
 		<Route path="/reduxPromiseAction2" component={reduxPromiseAction2} />
